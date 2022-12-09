ff8867178acdb566dacd39a0c9199eee453b2827
alifpdf <104689782+alifpdf@users.noreply.github.com>
2022-12-10 00:53:06 +0100

    Update main.c

--- main.c
+++ main.c
@@ -3,13 +3,15 @@
 #include <stdlib.h>
 #include "extension.h"
 #include <time.h> 
-int i;
-int k;
+
 
 
 
 
 int main(){
+int i;
+int k;
+int m;
 //----------------------------------nombre de ligne-----------------------------------------------------------------------------------------------------------------------------------------------
 	srand(time(NULL));
 	int nbligne=0;
@@ -64,53 +66,79 @@ int main(){
         		 
 //----------------------------------------------------------------ouverture de ficher et séparation de colonne-------------------------------------------------------------------------------   	
  	 	
-	int j=0;
+	int j=0; //c'est pour le while au moment de saisir de 1 à 8
 	FILE * fichier2=fopen("airbnb_donnees_propre.csv","r");
-	char texte2[256]; 
+	char texte2[256]; //enregistrer le texte
 	fgets(texte2,255,fichier2); //enregistrement des nom de colonne
-	char * colonne = strtok (texte2, "," );
+	char * colonne = strtok (texte2, "," );//decouper la colonne
 	double distance[nbligne];//on va ranger les distances 
 	int dist[nbligne];//on va ranger les distances en enlevant le négatif
 	int ordre[nbligne];// ranger les numéros de series issue de la première colonne
+	int prix[nbligne];//on va ranger les prix
 //------------------------------------------------------------------choix de calcul et le nombre k premier à utiliser-------------------------------------------------------------------------	
 	while(j==0 || j>8){
 		printf("vous voulez calculer quoi?\n");
 		printf("1-accommodates\n2-bedrooms\n3-bathrooms\n4-beds\n5-prix\n6-minimum_nights\n7-maximum_nights\n8-number_of_reviews\n");
-	scanf("%d",&j);}
+	scanf("%d",&j);
+	}
 	
 //-------------------------------------------------------------calcul de distance avec les autres appartement----------------------------------------------------------------------------------- 	
 	for(i=0;i<nbligne;i++)
 	{
 		fgets(texte2,255,fichier2);	//enregistrement des premieres données chiffrés
 		colonne = strtok (texte2, "," );//les données à couper
-		ordre[i]=atoi(colonne);
+		ordre[i]=atoi(colonne);//convertir le numéro de serie char en int
 		
 	
 		for(k=1;k<j+1;k++){       
-        	colonne = strtok (NULL, ",");} //séparation des données selon une virgule qui sépare	
+        	colonne = strtok (NULL, ",");
+        	if (k==5){
+        		prix[i]=atoi(colonne);//enregistrer le prix
+        	}
+        	} //séparation des données selon une virgule qui sépare	
         	
         	switch(j)
         	{
         		case 1:
         		distance[i]=(x1.accommodates-atoi(colonne));
+        		for(k=1;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){//enregistrer le prix
+        		prix[i]=atoi(colonne);
+        		}}
         		if(distance[i]<0)		//enlever le négatif
         		{dist[i]=distance[i]*(-1);}
         		else{dist[i]=distance[i];}
         		break;
         		case 2:
         		distance[i]=(x1.bedrooms-atoi(colonne));
+        		for(k=2;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){//enregistrer le prix
+        		prix[i]=atoi(colonne);
+        		}}
         		if(distance[i]<0)		//enlever le négatif
         		{dist[i]=distance[i]*(-1);}
         		else{dist[i]=distance[i];}
 			break;
 			case 3:
 			distance[i]=(x1.bathrooms-atoi(colonne));
+			for(k=3;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){//enregistrer le prix
+        		prix[i]=atoi(colonne);
+        		}}
 			if(distance[i]<0)		//enlever le négatif
         		{dist[i]=distance[i]*(-1);}
         		else{dist[i]=distance[i];}
 			break;
 			case 4:
 			distance[i]=(x1.beds-atoi(colonne));
+			for(k=4;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){//enregistrer le prix
+        		prix[i]=atoi(colonne);
+        		}}
 			if(distance[i]<0)		//enlever le négatif
         		{dist[i]=distance[i]*(-1);}
         		else{dist[i]=distance[i];}
@@ -138,126 +166,154 @@ int main(){
 			if(distance[i]<0)		//enlever le négatif
         		{dist[i]=distance[i]*(-1);}
         		else{dist[i]=distance[i];}
-			break;         			
-		 
+			break;         					 
+        	
         	}
+        	
         }	       		
     	
 	fclose(fichier2);
 	
   	
 //--------------------------------------------------------------------choix du k premier d'appartement et tri dans l'ordre décroissant-----------------------------------------------------------------
-	triRapid(dist,ordre,0,nbligne-1); //tri des distance dans l'ordre décroissant
+	triRapid(dist,ordre,prix,0,nbligne-1); //tri des distance dans l'ordre décroissant
 	int l;
 	int moyenne=0;
+	int moyenneprix=0;
 	printf("votre k nombre à afficher\n");
 	scanf("%d",&l);
 	for(i = 0; i < l; i++)  {
 	moyenne=moyenne+dist[i];//calcul de moyenne en faisant d'abord une somme
+	moyenneprix=moyenneprix+prix[i];
 	printf("%d:la distance est de %d\n",ordre[i],dist[i]);}//afficher les k premiers distances avec leurs numéros de séries
 	moyenne=moyenne/l;//calcul de moyenne
+	moyenneprix=moyenneprix/l;//calcul de moyenne
 	printf("la moyenne de distance est de %d\n",moyenne);//afficher la moyenne des k premiers données  
+	printf("la moyenne de prix est de %d\n",moyenneprix);//afficher la moyenne des k premiers données  
 //---------------------------de manière k aléatoire--------------------------------------------------------------------------------------------------------------------------------------------	
 	
-	int aleatoire;	
+	moyenne=0;
+	moyenneprix=0;
+	int aleatoire[l];
+	int z=0;	
 	FILE * fichier3=fopen("airbnb_donnees_propre.csv","r");
 	char texte3[256]; 
 	fgets(texte3,255,fichier3); //enregistrement des nom de colonne
 	char * colonne1 = strtok (texte3, "," );
-	double distancedesordre[nbligne];//on va ranger les distances aléatoirement
-	int distdesordre[nbligne]; //on va ranger les distances aléatoirement en enlevant le négatif
-	int desordre[nbligne];// ranger les numéros de series issue de la première colonne dans le désordre
+	double distancedesordre[l];//on va ranger les distances aléatoirement
+	int distdesordre[l]; //on va ranger les distances aléatoirement en enlevant le négatif
+	int desordre[l];// ranger les numéros de series issue de la première colonne dans le désordre
+	int prixale[l];// ranger les prix aléatoirement
+	for(m=0;m<l;m++)
+	{
+	aleatoire[m]=rand()%(nbligne+1);//générer le nombre aléatoire entre 0 et le nombre de données
+	}
+	triRapid1(aleatoire,0,l-1);//mettre les numéro aléatoire dns l'ordre decroissant
+	
 	for(i=0;i<nbligne;i++)
 	{
+		
 		fgets(texte3,255,fichier3);	//enregistrement des premieres données chiffrés
 		colonne1 = strtok (texte3, "," );//les données à couper
-		aleatoire=rand()%(nbligne);//générer le nombre aléatoire 0 et le nombre de données
-		desordre[aleatoire]=atoi(colonne1);//enregistrer le premier colonne qui contient les numéros de séries dans le désordre en convertissant en entier
-		
-		
-        	
-		for(k=1;k<j+1;k++){       
-        	colonne1 = strtok (NULL, ",");} //séparation des données selon une virgule qui sépare	
-        	switch(j){
+	    	if(i==aleatoire[z]) //nième ligne
+		{ 	desordre[z]=atoi(colonne1);//enregistrer le premier colonne qui contient les numéros de séries dans le désordre en convertissant en entier
+				        	
+			for(k=1;k<j+1;k++){       
+        			colonne1 = strtok (NULL, ",");
+        			if (k==5){
+        			prixale[z]=atoi(colonne);//enregistrer le prix
+        			}} //séparation des données selon une virgule qui sépare	
+        		switch(j){
         		case 1:
-        		distancedesordre[aleatoire]=(x1.accommodates-atoi(colonne1));
-        		if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+        		distancedesordre[z]=(x1.accommodates-atoi(colonne1));
+        		for(k=1;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){
+        		prixale[z]=atoi(colonne);//enregistrer le prix
+        		}}
+        		if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
         		break;
         		case 2:
-        		distancedesordre[aleatoire]=(x1.bedrooms-atoi(colonne1));
-        		if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+        		distancedesordre[z]=(x1.bedrooms-atoi(colonne1));
+        		for(k=2;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){
+        		prixale[z]=atoi(colonne);//enregistrer le prix
+        		}}
+        		if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
 			break;
 			case 3:
-			distancedesordre[aleatoire]=(x1.bathrooms-atoi(colonne1));
-			if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+			distancedesordre[z]=(x1.bathrooms-atoi(colonne1));
+			for(k=3;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){
+        		prixale[z]=atoi(colonne);//enregistrer le prix
+        		}}
+			if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
 			break;
 			case 4:
-			distancedesordre[aleatoire]=(x1.beds-atoi(colonne1));
-			if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+			distancedesordre[z]=(x1.beds-atoi(colonne1));
+			for(k=4;k<5;k++){       
+        		colonne = strtok (NULL, ",");
+        		if (k==4){
+        		prixale[z]=atoi(colonne);//enregistrer le prix
+        		}}
+			if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
 			break;
 			case 5:
-			distancedesordre[aleatoire]=(x1.prix-atoi(colonne1));
-			if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+			distancedesordre[z]=(x1.prix-atoi(colonne1));
+			if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
 			break;
 			case 6:
-			distancedesordre[aleatoire]=(x1.minimum_nights-atoi(colonne1));
-			if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+			distancedesordre[z]=(x1.minimum_nights-atoi(colonne1));
+			if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
 			break;
 			case 7:
-			distancedesordre[aleatoire]=(x1.maximum_nights-atoi(colonne1));
-			if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+			distancedesordre[z]=(x1.maximum_nights-atoi(colonne1));
+			if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
 			break;
 			case 8:
-			distancedesordre[aleatoire]=(x1.number_of_reviews-atoi(colonne1));
-			if(distancedesordre[aleatoire]<0)
-        		{distdesordre[aleatoire]=distancedesordre[aleatoire]*(-1);}
-        		else{distdesordre[aleatoire]=distancedesordre[aleatoire];}
+			distancedesordre[z]=(x1.number_of_reviews-atoi(colonne1));
+			if(distancedesordre[z]<0)//enlever le négatif
+        		{distdesordre[z]=distancedesordre[z]*(-1);}
+        		else{distdesordre[z]=distancedesordre[z];}
 			break;
 			default:
 			break;         			
-      
-        	
-        	}                                              
+      			}
+        		z=z+1;
+        	}
+        }                                              
         			 
-        }	       		
-    	
+        	       		   	
 	fclose(fichier3);
-
-//------------------------tri aleatoire--------------------------------------------------------------------------------------------------------------------------------------------------------	
+	triRapid(distdesordre,desordre,prixale,0,l-1); //tri des distance aleatoirement dans l'ordre décroissant
+	for(i = 0; i < l; i++)  {
+		moyenne=moyenne+distdesordre[i];//calcul de moyenne en faisant d'abord une somme
+		moyenneprix=moyenneprix+prixale[i];
+		printf("%d:la distance est de %d\n",desordre[i],distdesordre[i]);}//afficher les k premiers distances avec leurs numéros de séries
 	
-	
-	
-	int selection[l];//donnee en aléatoire
-	int selectionordre[l];//numéro de série
-	moyenne=0;
-	for(i=0;i<l;i++){		
-		selection[i]=distdesordre[i];//enregistrer les k premiers distances 
-		selectionordre[i]=desordre[i];//enregistrer les k premiers numéros de séries
-		moyenne=moyenne+selection[i];//calcul de moyenne en faisant d'abord une somme
-		}
 	moyenne=moyenne/l;//calcul de moyenne
-	triRapid(selection,selectionordre,0,l-1);//tri des k logements aléatoirement
-	printf("\naléatoire\n");
-        for(i = 0; i < l; i++)  {
-	printf("%d:la distance est de %d\n",selectionordre[i],selection[i]);//afficher les k premiers distances qui avait été rangé aléatoirement avec leurs numéros de séries
-	
-	}	
+	moyenneprix=moyenneprix/l;//calcul de moyenne
+	printf("la moyenne de distance est de %d\n",moyenne);//afficher la moyenne des k premiers données  
+	printf("la moyenne de prix est de %d\n",moyenneprix);//afficher la moyenne des k premiers données 
 
-	printf("la moyenne de distance aléatoirement est de %d\n",moyenne);//afficher la moyenne des k premiers données pris aléatoirement 
+
+	
     return 0;
 
 
@@ -269,6 +325,5 @@ int main(){
 	
 	
 	
-
 
 
