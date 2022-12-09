4114c5844a4269848883bbcdbb12f1544bc2b2cb
alifpdf <104689782+alifpdf@users.noreply.github.com>
2022-12-10 00:52:13 +0100

    Update extension.h

--- extension.h
+++ extension.h
@@ -1,11 +1,9 @@
 #ifndef EXTENSION_H
 #define EXTENSION_H
 
-//int nb_logement(int a);
-//int colonne(int j);
+
 void permuter(int *a, int *b);
-void triRapid(int tab[],int tab1[], int first, int last);
-
-
+void triRapid(int tab[],int tab1[],int tab2[], int first, int last);
+void triRapid1(int tab[], int first, int last);
 #endif
  
